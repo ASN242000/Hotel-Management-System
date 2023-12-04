42c072401db85da79d6ed634c439cf7b28167855
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2023-12-04 10:59:16 +0530

    Database Connection

---
+++ Hotel Management System/src/hotel/management/system/Dashboard.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.management.system;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.*;
+/**
+ *
+ * @author anusha
+ */
+public class Dashboard extends JFrame{
+    Dashboard(){
+        setBounds(0,0, 1920,1000);
+        setLayout(null);
+      
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1920, 1000, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,1920,1000);
+        add(image);
+        
+        JLabel text = new JLabel("THE TAJ GROUP WELCOMES YOU!");
+        text.setBounds(400, 80, 1000, 50);
+        text.setFont(new Font("Tahoma", Font.PLAIN, 46));
+        text.setForeground(Color.WHITE);
+        image.add(text);
+        
+        JMenuBar mb = new JMenuBar();
+        mb.setBounds(0,0,1920,30);
+        image.add(mb);
+        
+        JMenu hotel = new JMenu("HOTEL MANAGEMENT");
+        hotel.setForeground(Color.red);
+        mb.add(hotel);
+        
+        JMenuItem reception = new JMenuItem("RECEPTION");
+        hotel.add(reception);
+        
+        JMenu admin = new JMenu("ADMIN");
+        admin.setForeground(Color.blue);
+        mb.add(admin);
+        
+        JMenuItem addemployee = new JMenuItem("ADD EMPLOYEE");
+        admin.add(addemployee);
+        
+        JMenuItem addrooms = new JMenuItem("ADD ROOMS");
+        admin.add(addrooms);
+        
+        JMenuItem adddriver = new JMenuItem("ADD DRIVER");
+        admin.add(adddriver);
+        
+        setVisible(true);
+        
+    }
+    public static void main(String args[]){
+        new Dashboard();
+    }
+            
+    
+}
