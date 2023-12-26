29de4bcbeee3ebeee4254557fd0adaf8b76dd64a
ASN242000 <117072953+ASN242000@users.noreply.github.com>
2023-12-26 22:17:21 +0530

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -14,7 +14,7 @@ create table room(roomnumber varchar(10), availability varchar(20), cleaning_sta
 
 create table driver (name varchar(20), age varchar(10), gender varchar(15), company varchar(20), branch varchar(20), available varchar(20), location varchar(40));
 
-create table cutomer(document varchar(20), number varchar(30),name varchar(30), gender varchar(15),country varchar(20), room varchar(10), checkintime varchar(80), deposit varchar(20));
+create table customer(document varchar(20), name varchar(30),number varchar(30), gender varchar(15),country varchar(20), room varchar(10), checkintime varchar(80), deposit varchar(20));
 
 alter table cutomer rename customer;
 
@@ -22,3 +22,25 @@ desc customer;
 
 select * from room;
 
+create table department(department varchar(30), budget varchar(30));
+
+insert into department values ('Front Office','500000');
+insert into department values ('Housekeeping','40000');
+insert into department values ('Food and Beverage','23000');
+insert into department values ('Kitchen or Food Production','540000');
+insert into department values ('Security','320000');
+
+select * from department;
+select * from driver;
+select * from employee;
+
+select * from customer;
+
+update room set availability = 'Available' where roomnumber = '102';
+select * from room;
+
+delete from customer where document = "Passport";
+
+alter table driver rename column branch to brand;
+
+update room set availability = "Available" where roomnumber = '102';
